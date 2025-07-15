0bee8651bd9f134045fb5ad5de468a8cb8d56979
leo <leo@leo.com>
2025-07-15 15:53:52 +0800

    daily_update config

--- dump_qlib_bin.sh
+++ dump_qlib_bin.sh
@@ -10,7 +10,7 @@ fi
 
 mkdir -p $WORKING_DIR/dolt
 
-[ ! -d "$WORKING_DIR/dolt/investment_data" ] && cd $WORKING_DIR/dolt && dolt clone chenditc/investment_data
+[ ! -d "$WORKING_DIR/dolt/investment_data" ] && cd $WORKING_DIR/dolt && dolt clone leo_lee/investment_data
 [ ! -d "$WORKING_DIR/qlib" ] && git clone $QLIB_REPO "$WORKING_DIR/qlib"
 
 cd $WORKING_DIR/dolt/investment_data
